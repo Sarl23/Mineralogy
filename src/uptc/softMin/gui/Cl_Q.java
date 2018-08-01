1247a9efa1287bc1a9c0195f27dd5e7fa5c4c0f9
Alejandro <jimmyale3102@gmail.com>
2018-07-31 23:07:34 -0500

    download al3

--- src/uptc/softMin/gui/Cl_Q.java
+++ src/uptc/softMin/gui/Cl_Q.java
@@ -7,7 +7,11 @@ package uptc.softMin.gui;
 
 import java.awt.Image;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
 
 /**
  *
@@ -16,6 +20,14 @@ import javax.swing.JDialog;
 public class Cl_Q extends JDialog{
     
     private MainWindow mainWin;
+    private JButton btnGetRQD;    
+    //ComboBox
+    private JComboBox cbDescriptionRQD;
+    //Text Area
+    private JTextArea txvalueRQD;
+    //Label
+    private JLabel lbDescriptionRQD;
+    private JLabel lbValueRQD;
     
     
     public Cl_Q(MainWindow mainWin){
@@ -35,11 +47,25 @@ public class Cl_Q extends JDialog{
     }
     
     public void beginComponents(){
+        lbDescriptionRQD = new JLabel("Descripción");
+        lbDescriptionRQD.setBounds(30, 25, 100, 25);
+        cbDescriptionRQD = new JComboBox(new Object[] {"Muy mala", "Mala", "Regular", 
+        "Buena", "Excelente"});
+        cbDescriptionRQD.setBounds(30, 50, 80, 25);
         
+        lbValueRQD = new JLabel("Valor RQD");
+        lbValueRQD.setBounds(120, 25, 90, 25);
+        txvalueRQD = new JTextArea();
+        txvalueRQD.setBounds(120, 50, 60, 20);
+        
+        btnGetRQD = new JButton("Calcular RQD");
     }
     
     public void addComponents(){
-        
+        add(lbDescriptionRQD);
+        add(cbDescriptionRQD);
+        add(lbValueRQD);
+        add(txvalueRQD);
     }
     
 }
